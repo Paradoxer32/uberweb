98a1045b6821ce03cb42fac5645182f83de17fc1
Ahoora <paradoxer3232@gmail.com>
2023-06-19 11:28:22 +0000

    Set 'conf.py' configuration.

---
+++ templates/pythons/conf.py
@@ -0,0 +1,11 @@
+# Default host : '127.0.0.1' 
+host = '127.0.0.1' # localhost
+# host = '0.0.0.0' # Every hosts.
+
+# Default port : '8000'
+port = '8000'
+# port = '0000' # Every ports.
+
+# Default debug : True
+debug = True # Debug program.
+
